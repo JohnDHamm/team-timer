edaec8e829a8de0b6d85cf76395a6ba5305464f4
John Hamm <johndhamm1@gmail.com>
2016-10-26 15:07:58 -0500

    navbar added for testing page routes

---
+++ client/app/controllers/navCtrl.js
@@ -0,0 +1,12 @@
+"use strict";
+
+app.controller("navCtrl", function($scope, $location){
+
+		// $scope.logout = function(){
+		// AuthFactory.logout();
+		// $location.url("/login");
+		// // $scope.$apply();
+
+	// };
+
+});
