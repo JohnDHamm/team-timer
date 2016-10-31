269412390929f542e9ee2f08a50e171dcf006f21
John Hamm <johndhamm1@gmail.com>
2016-10-31 14:43:31 -0500

    fixed date in workouts table to integer, athletesArray of lapTimes created in workoutView

--- client/app/controllers/workoutViewCtrl.js
+++ client/app/controllers/workoutViewCtrl.js
@@ -1,13 +1,46 @@
 "use strict";
 
-app.controller("workoutViewCtrl", function($scope, $routeParams, DbFactory){
+app.controller("workoutViewCtrl", function($scope, $routeParams, DbFactory, WorkoutFactory){
 
 	console.log("params", $routeParams);
 	const date = $routeParams.date;
-	console.log("date", date);
+	$scope.displayDate = dateFormat(date);
+	// console.log("date", $scope.displayDate);
 
 	DbFactory.getWorkoutsByDate(date)
-		.then((workout) => {
-			console.log("workout: ", workout);
+		.then((workouts) => {
+			const workoutsArray = workouts;
+			console.log("workoutsArray: ", workoutsArray);
+			//set header info: total laps, lap_dist, lap_metric, discipline, date, descr?
+			$scope.totalLaps = workoutsArray[0].laps;
+			$scope.lap_distance = workoutsArray[0].lap_distance;
+			$scope.lap_metric = workoutsArray[0].lap_metric;
+			$scope.discipline = workoutsArray[0].discipline;
+			$scope.description = workoutsArray[0].description;
+
+			const athletesArray = WorkoutFactory.createAthletesArray(workouts);
+			console.log("athletesArray", athletesArray);
+
+
+
 		})
+
+
+
+
+
+
+
+
+	function dateFormat (date) {
+		console.log("date", date);
+		const newDate = new Date(1477447627584)
+		console.log("newDate", newDate);
+		const formattedDate = newDate;
+
+		return formattedDate;
+	}
+
+
+
 });
--- client/app/factories/WorkoutFactory.js
+++ client/app/factories/WorkoutFactory.js
@@ -17,6 +17,35 @@ app.factory("WorkoutFactory", function($q) {
 
 	const getCurrentWorkoutParams = () => currentWorkoutParams;
 
-	return { setCurrentWorkoutParams, getCurrentWorkoutParams};
+
+	const createAthletesArray = (workouts) => {
+		const athletesArray = [];
+		workouts.forEach(workout => {
+			let buildObj = {};
+			const lapTimesArray = buildLapTimesArray(workout);
+			buildObj.lapTimes = lapTimesArray;
+			buildObj.name = workout.display_name;
+			// console.log("buildObj", buildObj);
+			athletesArray.push(buildObj);
+
+		})
+
+		return athletesArray;
+
+	}
+
+	const buildLapTimesArray = (workout) => {
+		const newArray = workout.data.split(",");
+		const parsedArray = newArray.map((each) => {
+			return parseInt(each);
+		})
+		console.log("parsedArray", parsedArray);
+		return parsedArray;
+
+	}
+
+
+	return { setCurrentWorkoutParams, getCurrentWorkoutParams, createAthletesArray };
+
 
 });
