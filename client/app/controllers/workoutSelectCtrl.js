cfc4ee8ccfde0394b2a72c2fb9406348348ec14b
John Hamm <johndhamm1@gmail.com>
2016-11-01 11:57:08 -0500

    filters dates to array without dupes

--- client/app/controllers/workoutSelectCtrl.js
+++ client/app/controllers/workoutSelectCtrl.js
@@ -9,17 +9,31 @@ app.controller("workoutSelectCtrl", function($scope, UserFactory, DbFactory){
 		.then((workouts) => {
 			console.log("workouts", workouts);
 			$scope.workouts = workouts;
+			filterWorkouts(workouts);
 		})
 
 
 	const filterWorkouts = (workouts) => {
-		let filteredDates = [];
+		const allDates = [];
 		for (let i = 0; i < workouts.length; i++) {
-			let nextDate = workout[i].date;
+			allDates.push(workouts[i].date)
+		}
+		console.log("allDates", allDates);
+		const filteredDates = allDates.filter (function (value, index, array) {
+		    return array.indexOf (value) == index;
+		});
+
+
+
+		for (let i = 0; i < workouts.length; i++) {
 			for (let j = 0; j < filteredDates.length; j++) {
 
 			}
 		}
+
+
+		console.log("filteredDates", filteredDates);
+		return filteredDates;
 	};
 
 
