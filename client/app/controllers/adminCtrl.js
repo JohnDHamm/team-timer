8e2a9f577ad421d88d28fed52ad497e5b8381856
John Hamm <johndhamm1@gmail.com>
2016-11-01 23:23:29 -0500

    add new athlete started

--- client/app/controllers/adminCtrl.js
+++ client/app/controllers/adminCtrl.js
@@ -2,13 +2,15 @@
 
 app.controller("adminCtrl", function($scope, $routeParams, $location, UserFactory, DbFactory){
 
+	const team_id = 1;
 
 	Promise.resolve()
 		.then(() => UserFactory.getCurrentCoach())
 		.then((coach) => {
 			const currentCoach = coach;
-			const team_id = currentCoach.team_id;
-			$scope.team_id = currentCoach.team_id;
+			// const team_id = currentCoach.team_id;
+			$scope.team_id = team_id;
+			// $scope.team_id = currentCoach.team_id;
 			$scope.coachName = currentCoach.first_name;
 
 			return Promise.all([DbFactory.getTeamName(team_id), DbFactory.getGroupsByTeam(team_id), DbFactory.getAthletesByTeam(team_id)]);
@@ -48,6 +50,29 @@ app.controller("adminCtrl", function($scope, $routeParams, $location, UserFactor
 				$scope.newGroup_desc = "";
 				$scope.newGroup_name = "";
 			})
+	}
+
+	$scope.addAthlete = () => {
+		// const newGroup = {
+		// 	group_name: $scope.newGroup_name,
+		// 	description: $scope.newGroup_desc,
+		// 	team_id: $scope.team_id
+		// };
+		// console.log("newGroup", newGroup);
+		// DbFactory.addGroup(newGroup)
+		// 	.then(() => {
+		// 		reloadGroups();
+		// 	})
+	}
+
+	const reloadAthletes = () => {
+		// DbFactory.getGroupsByTeam($scope.team_id)
+		// 	.then((groups) => {
+		// 		$scope.groups = groups;
+		// 		// $scope.$apply();
+		// 		$scope.newGroup_desc = "";
+		// 		$scope.newGroup_name = "";
+		// 	})
 	}
 
 });
