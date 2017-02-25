71922fcb81eb86bad5257894807d67441932b33d
John Hamm <johndhamm1@gmail.com>
2017-02-25 15:28:37 -0600

    added swim + bike pace to migrations and seeds

--- db/migrations/20161024220849_createAthletesTable.js
+++ db/migrations/20161024220849_createAthletesTable.js
@@ -1,10 +1,12 @@
 module.exports.up = (knex, Promise) => {
   return knex.schema.createTable('Athletes', table => {
-  	table.increments() //creates an incrementing id as primary key
+  	table.increments()
   	table.string('first_name')
   	table.string('last_name')
   	table.string('display_name').unique()
   	table.integer('age')
+    table.integer('swim_pace')
+    table.integer('bike_pace')
   	table.integer('run_pace')
   	table.integer('group_id').references('Groups.id')
   })
--- db/seeds/d_add_athletes.js
+++ db/seeds/d_add_athletes.js
@@ -13,6 +13,8 @@ exports.seed = function(knex, Promise) {
 						display_name: 'Makenna',
 						age: 12,
 						group_id: 1,
+						swim_pace: 105000,
+						bike_pace: 17.7,
 						run_pace: 471000
 					}),
 				knex('Athletes')
@@ -23,6 +25,8 @@ exports.seed = function(knex, Promise) {
 						display_name: 'Lucy',
 						age: 12,
 						group_id: 1,
+						swim_pace: 107000,
+						bike_pace: 17.2,
 						run_pace: 450000
 					}),
 				knex('Athletes')
@@ -33,6 +37,8 @@ exports.seed = function(knex, Promise) {
 						display_name: 'Trinity',
 						age: 11,
 						group_id: 1,
+						swim_pace: 115000,
+						bike_pace: 16.7,
 						run_pace: 540000
 					}),
 				knex('Athletes')
@@ -43,6 +49,8 @@ exports.seed = function(knex, Promise) {
 						display_name: 'Wellington',
 						age: 12,
 						group_id: 1,
+						swim_pace: 102000,
+						bike_pace: 18.2,
 						run_pace: 475000
 					}),
 				knex('Athletes')
@@ -53,6 +61,8 @@ exports.seed = function(knex, Promise) {
 						display_name: 'Maya',
 						age: 10,
 						group_id: 1,
+						swim_pace: 112000,
+						bike_pace: 16.9,
 						run_pace: 502000
 					}),
 				knex('Athletes')
@@ -63,6 +73,8 @@ exports.seed = function(knex, Promise) {
 						display_name: 'Devin',
 						age: 9,
 						group_id: 2,
+						swim_pace: 120000,
+						bike_pace: 16.2,
 						run_pace: 572000
 					}),
 				knex('Athletes')
@@ -73,6 +85,8 @@ exports.seed = function(knex, Promise) {
 						display_name: 'Gus',
 						age: 13,
 						group_id: 2,
+						swim_pace: 127000,
+						bike_pace: 16.5,
 						run_pace: 497000
 					}),
 				knex('Athletes')
@@ -83,6 +97,8 @@ exports.seed = function(knex, Promise) {
 						display_name: 'Clara',
 						age: 12,
 						group_id: 1,
+						swim_pace: 116000,
+						bike_pace: 17.1,
 						run_pace: 486000
 					}),
 				knex('Athletes')
@@ -93,6 +109,8 @@ exports.seed = function(knex, Promise) {
 						display_name: 'Seamus',
 						age: 8,
 						group_id: 3,
+						swim_pace: 135000,
+						bike_pace: 14.9,
 						run_pace: 620000
 					}),
 				knex('Athletes')
@@ -103,6 +121,8 @@ exports.seed = function(knex, Promise) {
 						display_name: 'Emily Micah',
 						age: 6,
 						group_id: 3,
+						swim_pace: 135000,
+						bike_pace: 14.2,
 						run_pace: 695000
 					}),
 				knex('Athletes')
@@ -113,6 +133,8 @@ exports.seed = function(knex, Promise) {
 						display_name: 'Miles B',
 						age: 12,
 						group_id: 1,
+						swim_pace: 110000,
+						bike_pace: 17.5,
 						run_pace: 503000
 					}),
 				knex('Athletes')
@@ -123,6 +145,8 @@ exports.seed = function(knex, Promise) {
 						display_name: 'Ruth',
 						age: 13,
 						group_id: 1,
+						swim_pace: 123000,
+						bike_pace: 16.8,
 						run_pace: 563000
 					}),
 				knex('Athletes')
@@ -133,6 +157,8 @@ exports.seed = function(knex, Promise) {
 						display_name: 'Darby',
 						age: 9,
 						group_id: 2,
+						swim_pace: 118000,
+						bike_pace: 16.4,
 						run_pace: 585000
 					}),
 				knex('Athletes')
@@ -143,6 +169,8 @@ exports.seed = function(knex, Promise) {
 						display_name: 'Searcy',
 						age: 10,
 						group_id: 2,
+						swim_pace: 122000,
+						bike_pace: 16.8,
 						run_pace: 570000
 					}),
 				knex('Athletes')
@@ -153,6 +181,8 @@ exports.seed = function(knex, Promise) {
 						display_name: 'Jackson',
 						age: 13,
 						group_id: 2,
+						swim_pace: 125000,
+						bike_pace: 15.9,
 						run_pace: 620000
 					}),
 				knex('Athletes')
@@ -163,6 +193,8 @@ exports.seed = function(knex, Promise) {
 						display_name: 'Ryan',
 						age: 11,
 						group_id: 2,
+						swim_pace: 124000,
+						bike_pace: 16.2,
 						run_pace: 595000
 					}),
 
@@ -175,6 +207,8 @@ exports.seed = function(knex, Promise) {
 						display_name: 'Junior',
 						age: 11,
 						group_id: 4,
+						swim_pace: 100500,
+						bike_pace: 15.7,
 						run_pace: 492000
 					}),
 				knex('Athletes')
@@ -185,6 +219,8 @@ exports.seed = function(knex, Promise) {
 						display_name: 'Jonesy',
 						age: 12,
 						group_id: 4,
+						swim_pace: 100500,
+						bike_pace: 16.2,
 						run_pace: 497000
 					}),
 				knex('Athletes')
@@ -195,6 +231,8 @@ exports.seed = function(knex, Promise) {
 						display_name: 'Bobby F.',
 						age: 10,
 						group_id: 4,
+						swim_pace: 100500,
+						bike_pace: 16.2,
 						run_pace: 545000
 					})
 
